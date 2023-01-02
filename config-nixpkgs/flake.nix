f8b0e9bca504cf926ce47f563067b7bcdc7c3583
Ricardo Yepes <richard@empiric.us>
2023-01-02 15:35:40 +1100

    Initial setup with home-manager standalone

---
+++ config-nixpkgs/flake.nix
@@ -0,0 +1,36 @@
+{
+  description = "Home Manager configuration";
+
+  inputs = {
+    # Specify the source of Home Manager and Nixpkgs.
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  #### Agreagado self
+  outputs = { self, nixpkgs, home-manager, ... }:
+    let
+      system = "x86_64-linux";
+      pkgs = nixpkgs.legacyPackages.${system};
+      user = "ricardoyepes";
+    in {
+      homeConfigurations.${user} = home-manager.lib.homeManagerConfiguration {
+        inherit pkgs;
+
+        # Specify your home configuration modules here, for example,
+        # the path to your home.nix.
+        modules = [
+          ./home.nix
+        ];
+
+        # Optionally use extraSpecialArgs
+        # to pass through arguments to home.nix
+      };
+   #### Adaptado de https://github.com/mudrii/hmtst/blob/master/flake.nix
+      ${user} = self.homeConfigurations.${user}.activationPackage;
+      defaultPackage.${system} = self.${user};
+    };
+}
---
+++ config-nixpkgs/home.nix
@@ -0,0 +1,98 @@
+{ config, pkgs, ... }:
+
+let
+  user = "ricardoyepes";
+in
+{
+  # Home Manager needs a bit of information about you and the
+  # paths it should manage.
+  home.username = "${user}";
+  home.homeDirectory = "/Users/${user}";
+
+  # This value determines the Home Manager release that your
+  # configuration is compatible with. This helps avoid breakage
+  # when a new Home Manager release introduces backwards
+  # incompatible changes.
+  #
+  # You can update Home Manager without changing this value. See
+  # the Home Manager release notes for a list of state version
+  # changes in each release.
+  home.stateVersion = "22.11";
+
+  # Let Home Manager install and manage itself.
+  programs.home-manager.enable = true;
+
+
+  home.packages = with pkgs; [
+    vim
+    wget
+    bat
+    fd
+    ripgrep
+    gnugrep
+    tree
+    pstree
+    jq
+    yq
+    fzf
+    coreutils
+    htop
+    firefox
+    tmux
+    git
+    dotnet-sdk_7
+    fsharp
+    (let
+      my-python-packages = python-packages: with python-packages; [
+        toolz
+        ipython
+      ];
+      python-with-my-packages = python3.withPackages my-python-packages;
+    in
+    python-with-my-packages)
+  ];
+
+  programs.vscode = {
+    enable = true;
+    extensions = with pkgs.vscode-extensions; [
+      dracula-theme.theme-dracula
+      vscodevim.vim
+      yzhang.markdown-all-in-one
+      ms-python.python
+      ionide.ionide-fsharp
+      thenuprojectcontributors.vscode-nushell-lang
+      streetsidesoftware.code-spell-checker
+      ms-vscode.hexeditor
+      ms-vscode.PowerShell
+      ms-python.vscode-pylance
+      kahole.magit
+      jnoortheen.nix-ide
+      eamodio.gitlens
+      bbenoist.nix
+      b4dm4n.vscode-nixpkgs-fmt
+      vspacecode.whichkey
+      vspacecode.vspacecode
+    ];
+  }; 
+
+  home.file.".config/i3/config" = {
+    source = ./i3-config;
+  };
+
+  home.file.".zshrc" = {
+    source = ./zshrc;
+  };
+
+  home.file.".vimrc" = {
+    source = ./vimrc;
+  };
+
+  home.file.".gitconfig" = {
+    source = ./gitconfig;
+  };
+
+  home.file.".tmux.conf" = {
+    source = ./tmux.conf;
+  };
+
+}
