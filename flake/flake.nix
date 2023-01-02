f128de6d005f47c5ba8097e02247a690e001de78
Ricardo Yepes <10557463+ryepesg@users.noreply.github.com>
2023-01-02 19:40:39 +1100

    Home-manager as standalone installation

--- flake/flake.nix
+++ flake/flake.nix
@@ -3,10 +3,17 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    darwin = {
+      url = "github:lnl7/nix-darwin/master";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
-  # outputs = { self, nixpkgs, home-manager }:
-  outputs = { self, nixpkgs, ... }@attrs:
+  outputs = { self, nixpkgs, home-manager, darwin, ... }@attrs:
     let
       system = "x86_64-linux";
       pkgs = import nixpkgs {
@@ -14,7 +21,9 @@
         config.allowUnfree = true;
       };
       lib = nixpkgs.lib;
+      user = "ricardoyepes";
     in {
+
       nixosConfigurations = {
         ricardoyepes = lib.nixosSystem {
           inherit system;
@@ -22,5 +31,30 @@
           modules = [ ./configuration.nix ];
         };
       };
+
+#      darwinConfigurations = (                                              # Darwin Configurations
+#        import ./darwin {
+#          inherit (nixpkgs) lib;
+#          inherit inputs nixpkgs home-manager darwin user;
+#        }
+#      );
+
+      homeConfigurations = {
+        ${user} = home-manager.lib.homeManagerConfiguration {
+          inherit pkgs;
+          #username = "${user}";
+          #homeDirectory = "/home/${user}";
+
+          # home configuration modules
+          modules = [
+            ./home.nix
+          ];
+          # extraSpecialArgs to pass through arguments to home.nix
+        };
+      };
+
+      #${user} = self.homeConfigurations.${user}.activationPackage;
+      #defaultPackage.${system} = self.${user};
+
     };
 }
