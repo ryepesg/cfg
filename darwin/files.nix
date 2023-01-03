02a032ba9b73e22cbf8c05010fbe8f1d06336be8
Ricardo Yepes <10557463+ryepesg@users.noreply.github.com>
2023-01-04 09:41:56 +1100

    Adding RectangleConfig.json

--- darwin/files.nix
+++ darwin/files.nix
@@ -15,8 +15,14 @@
     source = ./os.sh;
   };
 
+  # Rectangle is able to create the plist from "~/Library/Application Support/Rectangle/RectangleConfig.json"
+  # Using the JSON instead of the plist because it is easier to track the changes
   home.file."Library/Preferences/com.knollsoft.Rectangle.plist" = {
     source = ./preferences/com.knollsoft.Rectangle.plist;
+  };
+
+  home.file."Library/Application Support/Rectangle/RectangleConfig.json" = {
+    source = ./preferences/RectangleConfig.json;
   };
 
 }
