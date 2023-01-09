c3fc91cfed76b56786f955695f0e6faa2dfeb965
Ricardo Yepes <richard@empiric.us>
2023-01-10 00:10:05 +1100

    zsh configuration

--- flake/programs/i3/default.nix
+++ flake/programs/i3/default.nix
@@ -5,10 +5,10 @@
   xdg.configFile."i3/config".source = ./i3.config;
   #xdg.configFile."i3status/config".source = ./i3status.config;
 
-  services.screen-locker = {
-    enable = true;
-    inactiveInterval = 5;
-    lockCmd = "${pkgs.i3lock}/bin/i3lock -f -c 000000";
-  };
+  # services.screen-locker = {
+  #   enable = true;
+  #   inactiveInterval = 5;
+  #   lockCmd = "${pkgs.i3lock}/bin/i3lock -f -c 000000";
+  # };
 
 }
