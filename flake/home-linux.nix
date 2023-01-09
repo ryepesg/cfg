1a5a9575ef3925d445792502db067c1b219c8532
Ricardo Yepes <10557463+ryepesg@users.noreply.github.com>
2023-01-10 02:07:00 +1100

    Fix xdg in MacOS

---
+++ flake/home-linux.nix
@@ -0,0 +1,18 @@
+{ config, pkgs, user, homedir, ... }:
+
+{
+
+  xdg.userDirs = {
+    enable = true;
+    createDirectories = true;
+    desktop = "$HOME/xdg/desktop";
+    download = "$HOME/xdg/downloads";
+    documents = "$HOME/xdg/documents";
+    music = "$HOME/xdg/music";
+    pictures = "$HOME/xdg/pictures";
+    videos = "$HOME/xdg/videos";
+    templates = "$HOME/xdg/templates";
+    publicShare = "$HOME/xdg/share";
+  };
+
+}
