f398af2a1fa91bd97f37ad6c2807ccdd073087a1
Ricardo Yepes <richard@empiric.us>
2023-01-10 00:23:37 +1100

    Pagger

--- flake/home.nix
+++ flake/home.nix
@@ -213,5 +213,21 @@
     };
   };
 
+  home.sessionVariables = {
+    PAGER = "less -R";
+  };
+
+  xdg.configFile."user-dirs.dirs".text = ''
+    XDG_DESKTOP_DIR="$HOME/xdg/desktop"
+    XDG_DOWNLOAD_DIR="$HOME/xdg/downloads"
+    XDG_DOCUMENTS_DIR="$HOME/xdg/documents"
+    XDG_MUSIC_DIR="$HOME/xdg/music"
+    XDG_PICTURES_DIR="$HOME/xdg/pictures"
+    XDG_VIDEOS_DIR="$HOME/xdg/videos"
+    XDG_TEMPLATES_DIR="$HOME/xdg/templates"
+    XDG_PUBLICSHARE_DIR="$HOME/xdg/share"
+  '';
+
+  systemd.user.startServices = true;
 
 }
