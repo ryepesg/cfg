d6629e7c248e2243a5f2b901c9e01f2e9bbcddc8
Ricardo Yepes <10557463+ryepesg@users.noreply.github.com>
2026-06-02 11:56:04 +1000

    update neovim configs

--- users/programs/editors/nvim/default.nix
+++ users/programs/editors/nvim/default.nix
@@ -10,6 +10,8 @@
       enable = true;
       viAlias = true;
       vimAlias = true;
+      withRuby = false;
+      withPython3 = false;
 
       plugins = with pkgs.vimPlugins; [
         # Syntax
